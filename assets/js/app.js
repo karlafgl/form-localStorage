a3a9335da4312238d5b7913850ce3424eab628f6
Karlafgl <karlafgl@gmail.com>
2019-02-13 11:08:49 -0700

    Agregando funcionalidad al boton agregar tweet y visualizarlo en pantalla

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,21 @@ function eventListeners(){
 //Agregar tweet del formulario
 function agregarTweet(e) {
     e.preventDefault();
-    console.log("form enviada")
+    // console.log("form enviada")
+
+    //leer el valor del textarea
+    const textTweet = document.getElementById("tweet").value
+    // console.log(textTweet)
+
+    //crear elemento y anadir el elemento a la lista 
+    const li = document.createElement('li')
+    //crear boton borrar 
+    const botonBorrar = document.createElement('a')
+    //agrega el input en el li
+    li.textContent = textTweet
+    //agrega la clase a la X 
+    botonBorrar.classList = 'borrar-tweet'
+    botonBorrar.innerText = 'X'
+    listaTweets.appendChild(li)
+    li.appendChild(botonBorrar)
 }
-
-
